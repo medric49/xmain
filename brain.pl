aee43b3cb27e8ee2875c398921559dbf52bc5a93
Kevin Nivekiba <kevinnguetchouang@gmail.com>
2019-03-04 21:35:54 +0100

    Test local

--- brain.pl
+++ brain.pl
@@ -7,29 +7,31 @@ sendSolution(Solution) :- term_to_atom(Solution, S),
 hypothesis("changer de chargeur") :- chargeur, !.
 hypothesis("changer le circuit d'alimentation") :- alimentation, !.
 hypothesis("bien connecter le bouton d'allumage ou le changer") :- boutondallumage, !.
-hypothesis("bien connecter le processeur et le ventilateur, les changer si le probl�me persiste") :- processeur, !.
+hypothesis("bien connecter le processeur et le ventilateur, les changer si le problème persiste") :- processeur, !.
 hypothesis("nettoyer le ventilateur et si cela ne fonctionne pas en changer") :- ventilateur, !.
-hypothesis("bien r�gler la luminosit� de l'�cran") :- luminosite, !.
-hypothesis("changer l'�cran") :- ecran, !.
-hypothesis("r�chauffer la carte graphique, la changer si le probl�me persiste") :- cartegraphique, !.
-hypothesis("refaire le syst�me d'exploitation, si le probl�me persiste changer de disque dur") :- disquedur, !.
+hypothesis("bien régler la luminosité de l'écran") :- luminosite, !.
+hypothesis("changer l'écran") :- ecran, !.
+hypothesis("r�chauffer la carte graphique, la changer si le problème persiste") :- cartegraphique, !.
+hypothesis("refaire le système d'exploitation, si le problème persiste changer de disque dur") :- disquedur, !.
 hypothesis("placer correctement le disque dur") :- disquedurmalplace, !.
+hypothesis("Re-installer le logiciel") :- logiciel, !.
 hypothesis("bien placer les barrettes RAM, les changer une � une si le probl�me persiste") :- ram, !.
 hypothesis("nettoyer les barrettes RAM, les changer si le probl�me persiste") :- ram2, !.
 hypothesis("vérifier la nappe de connexion du clavier, ou alors utiliser un clavier externe") :- clavier, !.
-hypothesis(".....nous ne parvenons pas � d�tecter votre probl�me"). /*Pas de diagnostic*/
+hypothesis(".....nous ne parvenons pas à détecter votre problème"). /*Pas de diagnostic*/
 
 /*Règles de dépannage*/
-ventilateur :- verify("s'allume"), verify("s'arr�te juste apr�s").
-chargeur :- not(verify("s'allume")), verify("s'allume apr�s avoir changé de chargeur").
-processeur :- not(verify("s'allume")), verify("�met 5 bips longs").
-luminosite :- verify("s'allume"), verify("a l'�cran tout noir ou qui présente des taches"), verify("donne des images apr�s avoir bien r�gl� la luminosit�").
+ventilateur :- verify("s'allume"), verify("s'arrête juste après").
+chargeur :- not(verify("s'allume")), verify("s'allume après avoir changé de chargeur").
+processeur :- not(verify("s'allume")), verify("émet 5 bips longs").
+luminosite :- verify("s'allume"), verify("a l'écran tout noir ou qui présente des taches"), verify("donne des images apr�s avoir bien r�gl� la luminosit�").
 ecran :- verify("s'allume"), not(verify("s'arrête juste après")), verify("a l'écran tout noir ou qui présente des taches"), not(verify("donne des images après avoir bien réglé la luminosité")), verify("donne des images sur un moniteur externe").
 ram :- verify("s'allume"), verify("émet 1 bip long suivi d'1 bip court ou alors des bips longs uniquement").
 cartegraphique:- (verify("s'allume"), verify("a l'écran tout noir ou qui présente des taches"), not(verify("donne des images sur un moniteur externe")), not(verify("donne des images claires après avoir nettoyé ou changer les RAM"))) ; (verify("s'allume") , verify("émet 1 bip long suivi de 2 bips courts")).
 ram2 :- verify("s'allume"), verify("a l'écran tout noir ou qui présente des taches"), not(verify("donne des images sur un moniteur externe")), verify("donne des images claires après avoir nettoyé ou changer les RAM").
 disquedur :- verify("s'allume"), not(verify("finit le chargement du système d'exploitation(ne tourne pas indéfiniment)")).
 disquedurmalplace :- verify("s'allume"), verify("affiche le message d'erreur boot device not found").
+logiciel :- verify("Est-ce que la RAM est pleine ?"), verify("Est-ce que tu as fait").
 clavier :- verify("s'allume"), not(verify("affiche les touches que nous saisissons au clavier")).
 alimentation :- not(verify("s'allume")), not(verify("s'allume après avoir bien connecté le bouton d'allumage ou après l'avoir changé")).
 boutondallumage :- not(verify("s'allume")).
